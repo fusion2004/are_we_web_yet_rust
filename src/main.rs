20b1ade9808a334f75e306c09d5cc7372cfef6f4
Mark Oleson <fusion2004@gmail.com>
2016-08-11 14:54:57 -0400

    use chan & chan_signal to receive signals and print a message before exiting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+#[macro_use]
+extern crate chan;
+extern crate chan_signal;
 extern crate clap;
 extern crate iron;
 extern crate router;
 extern crate serde;
 extern crate serde_json;
 
+use chan_signal::Signal;
 use clap::{Arg, App};
 use iron::{Iron};
 
@@ -12,6 +16,30 @@ mod models;
 mod routes;
 
 fn main() {
+    // Signal gets a value when the OS sent a INT or TERM signal.
+    let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
+    // When our work is complete, send a sentinel value on `sdone`.
+    let (sdone, rdone) = chan::sync(0);
+    // Run work.
+    ::std::thread::spawn(move || run(sdone));
+
+    // Wait for a signal or for work to be done.
+    chan_select! {
+        signal.recv() -> signal => {
+            match signal.unwrap() {
+                Signal::INT | Signal::TERM => {
+                    println!("");
+                    println!("Shutting down")
+                },
+                _ => {}
+            }
+        },
+        rdone.recv() => {
+        }
+    }
+}
+
+fn run(_sdone: chan::Sender<()>) {
     let arg_matches = App::new("are_we_web_yet")
         .version("0.1")
         .author("Mark O. <fusion2004@gmail.com>")
